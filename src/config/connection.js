77633b846ea6cf30e9d488a580698065d98b0ff3
Thiago Pereira <thiago7ps@outlook.com>
2022-11-04 22:23:19 -0300

    ok

--- src/config/connection.js
+++ src/config/connection.js
@@ -6,21 +6,3 @@ const sequelize = new Sequelize("todolist", "root", "", {
 });
 
 module.exports = sequelize;
-
-// const test = async function() {
-//   let sql = `SELECT * FROM tasks`
-//   return await sequelize.query(sql, {type: sequelize.QueryTypes.SELECT })
-//   .then((row) => {
-//     if(row && row[0]) {
-//       return row[0]
-//     } else {
-//       return false
-//     }
-//   })
-// }
-
-// const start = async function() {
-//   const result = await test();
-
-//   console.log(result);
-// }
