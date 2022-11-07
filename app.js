0ef9bdff34f8f07f290868c5c555eb8e9d7be349
Thiago Pereira <thiago7ps@outlook.com>
2022-11-07 18:04:24 -0300

    add method-override

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+//const
 const routes = require('./routes/routes');
 const express = require('express');
 const path = require('path');
-
+const methodOverride = require('method-override');
 const app = express();
+
+//use
 app.use(express.json());
-
 app.use('/public/', express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
+app.use(routes);
 
+//set
 app.set('views', path.join(__dirname, './src/views'));
 app.set('view engine', 'ejs');
 
@@ -14,4 +19,3 @@ app.listen(3000, () => {
   console.log('The server has started!');
 });
 
-app.use(routes);
