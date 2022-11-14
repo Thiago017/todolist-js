ee012112d06e6433bfecfeeb89d14606307e9712
Thiago Pereira da Silva <thiago7ps@outlook.com>
2022-11-14 10:48:36 -0300

    Disable sequelize logs

--- src/config/connection.js
+++ src/config/connection.js
@@ -2,7 +2,8 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize("todolist", "root", "", {
   host: "localhost",
-  dialect: "mariadb"
+  dialect: "mariadb",
+  logging: false //Set false for disable query logs
 });
 
 module.exports = sequelize;
